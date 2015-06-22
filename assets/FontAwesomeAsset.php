ef8fa1d3665c41241c11b9a340dc7671b3e11fe2
Sathit Seethaphon <dixonsatit@gmail.com>
2015-06-22 10:08:14 +0700

    init

---
+++ assets/FontAwesomeAsset.php
@@ -0,0 +1,18 @@
+<?php
+namespace dixonsatit\agencyTheme\assets;
+
+use yii\web\AssetBundle;
+
+class FontAwesomeAsset extends AssetBundle 
+{
+    public $sourcePath = '@bower/fontawesome'; 
+    public $css = [ 
+        'css/font-awesome.min.css', 
+    ];
+    public $publishOptions = [
+        'only' => [
+            'fonts/',
+            'css/',
+        ]
+    ];
+}
